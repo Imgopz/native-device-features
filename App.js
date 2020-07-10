f2cd2c1482f1702b381c5548ccd8d3947dc2b737
Imgopz <rrgkmech@gmail.com>
2020-07-10 23:16:25 +0530

    configuring redux, and adding reducer and action to add a place

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 
 import PlacesNavigator from './navigation/PlacesNavigator';
+import placesReducer from './store/places-reducer';
+
+const rootReducer = combineReducers({
+  places: placesReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <PlacesNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <PlacesNavigator />
+      </NavigationContainer>
+    </Provider>
   )
 }
 
